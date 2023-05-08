3ad2519a6c66b9d9df10ae03ec57375b48896643
김윤욱 <reznov03048@gmail.com>
2023-05-08 22:14:06 +0900

    Init commit

---
+++ src/main/java/toyboard/yoon/YoonApplication.java
@@ -0,0 +1,13 @@
+package toyboard.yoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyboard/yoon/YoonApplicationTests.java
@@ -0,0 +1,13 @@
+package toyboard.yoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
