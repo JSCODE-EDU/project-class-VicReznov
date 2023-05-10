c68569390d3bcf50707bbb9564bbbf39b333e7bf
김윤욱 <reznov03048@gmail.com>
2023-05-10 20:11:28 +0900

    feat: Article을 createdAt기준으로 내림차순 정렬, 최대 100개 조회하는 repository 기능 구현

--- src/main/java/toyboard/yoon/repository/ArticleRepository.java
+++ src/main/java/toyboard/yoon/repository/ArticleRepository.java
@@ -1,10 +1,15 @@
 package toyboard.yoon.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import toyboard.yoon.domain.Article;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    List<Article> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
