87e1847622a642fbb3b25ea76f95ad6dba544f1a
김윤욱 <reznov03048@gmail.com>
2023-05-10 20:09:51 +0900

    feat: ArticleDto를 List로 만드는 기능 구현

--- src/main/java/toyboard/yoon/mapper/ArticleMapper.java
+++ src/main/java/toyboard/yoon/mapper/ArticleMapper.java
@@ -3,7 +3,9 @@ package toyboard.yoon.mapper;
 import toyboard.yoon.domain.Article;
 import toyboard.yoon.dto.ArticleDto;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ArticleMapper {
 
@@ -14,10 +16,21 @@ public class ArticleMapper {
         articleDto.setTitle(article.getTitle());
         articleDto.setContents(article.getContents());
         articleDto.setAuthor(article.getAuthor());
-        articleDto.setPassword(article.getPassword());
+//        articleDto.setPassword(article.getPassword());
         articleDto.setCreatedAt(article.getCreatedAt());
 
         return articleDto;
+    }
+
+    public static List<ArticleDto> articleToDtos(List<Article> articles) {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+
+        for(Article article : articles) {
+            ArticleDto articleDto = articleToDto(article);
+            articleDtos.add(articleDto);
+        }
+
+        return articleDtos;
     }
 
     public static Article dtoToArticle(ArticleDto articleDto) {
