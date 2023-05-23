6f1356c57a5999507e1aaf2035230540f482908f
김윤욱 <reznov03048@gmail.com>
2023-05-23 22:52:11 +0900

    feat: MemberRepository

--- src/main/java/toyboard/yoon/repository/MemberRepository.java
+++ src/main/java/toyboard/yoon/repository/MemberRepository.java
@@ -1,13 +1,13 @@
 package toyboard.yoon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-import toyboard.yoon.domain.Member;
+import toyboard.yoon.entity.Member;
 
 import java.util.Optional;
 
-@Transactional
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByEmailAndPassword(String email, String password);
 }
