11abd05910e7b6182ef162de5725acf6bba81505
김윤욱 <reznov03048@gmail.com>
2023-05-28 13:38:43 +0900

    feat: jwt 회원가입, 로그인

--- src/main/java/toyboard/yoon/controller/MemberController.java
+++ src/main/java/toyboard/yoon/controller/MemberController.java
@@ -1,51 +1,49 @@
 package toyboard.yoon.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import toyboard.yoon.config.security.JwtConfig;
-import toyboard.yoon.dto.member.MemberRequestDto;
-import toyboard.yoon.dto.member.MemberResponseDto;
-import toyboard.yoon.service.member.impl.MemberServiceImpl;
+import toyboard.yoon.entity.Member;
+import toyboard.yoon.enumeration.MemberRole;
+import toyboard.yoon.repository.MemberRepository;
+import toyboard.yoon.security.JwtTokenProvider;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-@RestController
-@RequestMapping("/api/member")
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class MemberController {
 
-    private final MemberServiceImpl memberService;
-    private final JwtConfig jwtConfig;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
 
-    @PostMapping("/signup")
-    public MemberResponseDto createMember(MemberRequestDto memberRequestDto) {
-        return memberService.createMember(memberRequestDto);
+    // 회원가입
+    @PostMapping("/register")
+    public Long register(@RequestBody Map<String, String> user) {
+        return memberRepository.save(Member.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .memberRole(MemberRole.USER)
+                .build()).getId();
     }
 
-    @GetMapping("/my")
-    public MemberResponseDto findMember(Authentication authentication) {
-        if (authentication == null) {
-            throw new BadCredentialsException("회원 정보를 찾을 수 없습니다.");
-        }
-        return memberService.findMember(authentication.getName());
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')") // 권한을 통제
-    public List<MemberResponseDto> findAllMember() {
-        return memberService.findAll();
-    }
-
+    // 로그인
     @PostMapping("/login")
-    public String login(MemberRequestDto memberRequestDto) {
-        MemberResponseDto users = memberService.findByEmailAndPassword(memberRequestDto.getEmail(), memberRequestDto.getPassword());
-        return jwtConfig.createToken(users.getEmail(), Arrays.asList(users.getMemberRole().getValue()));
+    public String login(@RequestBody Map<String, String> user) {
+        Member member = memberRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입 되지 않은 이메일입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호가 맞지 않습니다.");
+        }
+
+        return jwtTokenProvider.createToken(member.getEmail(), member.getMemberRole());
     }
+
+
 }
